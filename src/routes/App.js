19a94d0a4c60c904ea980020d8664bdf5df896f7
adcastiblanco <angelcastiblanco96@gmail.com>
2020-12-03 18:11:37 -0500

    add pages folder

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
+
+import Home from "../pages/Home";
 
 function App() {
-  return <h1> Hello World </h1>;
+  return (
+    <BrowserRouter>
+      <Route path="/" component={Home} />
+    </BrowserRouter>
+  );
 }
 
 export default App;
